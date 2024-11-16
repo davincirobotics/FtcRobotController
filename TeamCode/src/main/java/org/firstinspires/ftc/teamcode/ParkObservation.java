131dc761cf80f0a9423e34001d45bdb1b2524e8c
aratof <aratof@framework.localdomain>
2024-11-16 05:57:05 -0700

    auto done easy cuz yippie yay"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkObservation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkObservation.java
@@ -56,7 +56,7 @@ public class BlueLeftPurpleAndYellow extends LinearOpMode {
 
 	DistanceSensor distance;
 
-	public double robotSpeed=0.3;
+	public double robotSpeed=0.5;
 
 	public double encoderPulses; //number of ticks needed to move
 	public double wheelCircumference = 30.159; //circumference of wheel
@@ -121,9 +121,7 @@ public class BlueLeftPurpleAndYellow extends LinearOpMode {
 			//telemetry.addLine("lowered gripper");
 			//telemetry.update();
 			
-                        rotateVer2(90,1,0.25);
-			moveForwardCM(100.0);
-			sleep(500);
+			moveForwardCM(50.0);
 		}
 	}
 		
